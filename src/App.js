81a99d36528f01e2ee1f696be4281dbf6825dcf5
razvanbucur360 <razvanbucur360@gmail.com>
2021-10-12 21:31:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          in final merge cum trebuieeeeeeeeyeyeye
         </p>
         <a
           className="App-link"
